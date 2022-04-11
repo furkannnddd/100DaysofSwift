f6b64345cbf62585191256dde1e0624f7e9b8d77
Furkan DURSUN <furkandursun@Furkan-MacBook-Air.local>
2022-04-12 00:58:46 +0300

    100DaysOfSwift

---
+++ 09_GrandCentralDispatch/project07/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SceneDelegate.swift
+//  project07
+//
+//  Created by Furkan on 3.04.2022.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var detailItem: Petition?
+
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let detailItem = detailItem else { return }
+
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        <h3>\(detailItem.title)</h3>
+        <p>\(detailItem.body)</p>
+        </body>
+        </html>
+        """
+
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+}
---
+++ 09_GrandCentralDispatch/project07/ViewController.swift
@@ -0,0 +1,119 @@
+//
+//  SceneDelegate.swift
+//  project07
+//
+//  Created by Furkan on 3.04.2022.
+//
+
+
+import UIKit
+
+class ViewController: UITableViewController {
+	var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
+    
+	override func viewDidLoad() {
+		super.viewDidLoad()
+        let filterButton = UIBarButtonItem(title: "-Filter-", style: .plain, target: self, action: #selector(filterPetitions))
+        let resetButton = UIBarButtonItem(title: "-Reset-", style: .plain, target: self, action: #selector(resetList))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        spacer.width = 40.0
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+        navigationItem.leftBarButtonItems = [filterButton, spacer, resetButton]
+        
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+	}
+
+    @objc func fetchJSON() {
+
+        
+        let urlString: String
+
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    parse(json: data)
+                    return
+            }
+        }
+            
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            filteredPetitions = petitions
+            tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        } else {
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+        }
+    }
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return filteredPetitions.count
+	}
+    
+    @objc func resetList(action: UIAlertAction) {
+        filteredPetitions = petitions
+        tableView.reloadData()
+    }
+    
+    @objc func filterPetitions() {
+            let ac = UIAlertController(title: "Filter petitions", message: "Type in to filter...", preferredStyle: .alert)
+            ac.addTextField()
+            
+            let filterAction = UIAlertAction(title: "Filter", style: .default) {
+                [weak self, weak ac] _ in
+                guard let filterWord = ac?.textFields?[0].text else { return }
+                self?.performSelector(inBackground: #selector(self?.showPetitions(for:)), with: filterWord)
+                self?.tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+            }
+            
+            ac.addAction(filterAction)
+            present(ac, animated: true)
+        }
+    
+    @objc func showPetitions(for filter: String) {
+        filteredPetitions = filteredPetitions.filter { $0.title.lowercased().contains(filter) }
+        }
+        
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "We The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let petition = filteredPetitions[indexPath.row]
+        
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        
+        return cell
+	}
+
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let vc = DetailViewController()
+		vc.detailItem = petitions[indexPath.row]
+		navigationController?.pushViewController(vc, animated: true)
+	}
+
+	@objc func showError() {
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+
